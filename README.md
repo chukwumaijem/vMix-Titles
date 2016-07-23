3a30aed6e1a84d174b8bd5c1803ace6d0c015d91
Ezumezu <aezumezu@gmail.com>
2016-07-24 00:47:32 +0100

    add UpNext and ComingUpNext titles

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# vMix-Titles
+# Free vMix Titles
+
+Feel free to use and modify the titles to suit your need.
+Dont forget to star the repo and add your own titles for others to benefit.
---
+++ UpNext/UpNext.xaml.vb
@@ -0,0 +1,21 @@
+Imports System
+Imports System.Collections.Generic
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Media.Imaging
+Imports System.Windows.Navigation
+Imports System.Windows.Shapes
+
+Partial Public Class ComingUpNext.xaml
+	Public Sub New()
+		MyBase.New()
+
+		Me.InitializeComponent()
+
+		' Insert code required on object creation below this point.
+	End Sub
+End Class
